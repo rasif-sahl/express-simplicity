b1481f3ac6b0b332c89faf14773346bff038f6e4
Rasif Sahl <rasifsahl@Rasifs-MacBook-Pro.local>
2023-06-04 22:05:27 +0530

    code has been updated

--- scripts/setup.js
+++ scripts/setup.js
@@ -1,22 +1,65 @@
-import { mkdirSync } from 'fs';
+import fs from 'fs';
 import { join } from 'path';
 
 const createFolderStructure = (baseDir, structure) => {
-  Object.entries(structure).forEach(([folderName, subStructure]) => {
-    const folderPath = join(baseDir, folderName);
-    mkdirSync(folderPath);
+  Object.entries(structure).forEach(([name, content]) => {
+    const itemPath = join(baseDir, name);
 
-    if (subStructure) {
-      createFolderStructure(folderPath, subStructure);
+    if (typeof content === 'object') {
+      fs.mkdirSync(itemPath);
+      createFolderStructure(itemPath, content);
+    } else if (typeof content === 'string') {
+      fs.writeFileSync(itemPath, content);
     }
   });
 };
 
 const baseDir = process.cwd();
 const structure = {
-  src: {},
+  src: {
+    'controllers': {},
+    'models': {},
+    'routes': {},
+    'services': {},
+    'example.js' : 'console.log("Hello from example.js");',
+  },
   config: {},
-  tests: {}
+  public: {},
+  'myfile.txt': 'Content for myfile.txt',
 };
 
 createFolderStructure(baseDir, structure);
+
+// const filePath = join(baseDir, 'myfile.txt');
+// const fileContent = 'Content for myfile.txt';
+
+// writeFileSync(filePath, fileContent);
+
+// console.log('File created:', filePath);
+
+
+const files = [
+  {
+    name: 'myfile.txt',
+    content: 'Content for myfile.txt',
+  },
+  {
+    name: 'example.js',
+    content: 'console.log("Hello from example.js");',
+    directory: 'src', // Specify the subdirectory path
+  },
+];
+
+
+files.forEach((file) => {
+  const filePath = join(baseDir, file.directory || '', file.name);
+  fs.writeFileSync(filePath, file.content);
+  console.log('File created:', filePath);
+});
+
+
+// files.forEach((file) => {
+//   const filePath = path.join(baseDir, file.directory, file.name);
+//   fs.writeFileSync(filePath, file.content);
+//   console.log('File created:', filePath);
+// });

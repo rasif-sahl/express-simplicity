80060435eaf87a2445cdbef1e3b988901f64a81c
Rasif Sahl <rasifsahl@Rasifs-MacBook-Pro.local>
2023-06-15 20:57:58 +0530

    file structure updates are done

---
+++ src/files/landing.js
@@ -0,0 +1,12 @@
+const getLandingPage = () => {
+    return `<!DOCTYPE html>
+    <html>
+        <body>
+            <h1>Hello Express</h1>
+        </body>
+    </html>`
+};
+
+module.exports = {
+    getLandingPage
+};
